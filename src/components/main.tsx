5d86d9f3aae8d5acf99953eedd7c9bd5797b623b
maudlyn01 <maudana2000@gmail.com>
2025-05-09 16:59:06 +0200

    update:types of goods

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -2,18 +2,25 @@ export const Main = () => {
   return (
     <main>
       <h3>Temos disponíveis os seguintes artigos:</h3>
-        <figure>
-          <a href="https://tech4u.co.mz/produto/acer-chromebook-315/">
-            <img src="https://m.media-amazon.com/images/I/71kmViEHobL._AC_UF894,1000_QL80_.jpg" alt="laptop accer" width="500" height="500" />
-           <p>Acer ChromeBook 315</p>
-        </a>
-        </figure>
-         <figure>
-          <a href="https://tech4u.co.mz/produto/acer-chromebook-315/">
-            <img src="https://cdn.uc.assets.prezly.com/5b79cc3a-6778-4bc7-a620-346a387dc95d/aspire-vero_16-03.jpg" alt="laptop accer" width="500" height="500" />
-           <p>Aspirep</p>
-        </a>
-        </figure>
+        <a href="https://tech4u.co.mz/produto/acer-chromebook-315/"><h4>Laptops</h4> </a>
+        <img src="https://m.media-amazon.com/images/I/71kmViEHobL._AC_UF894,1000_QL80_.jpg" alt="laptop accer" width="200" height="200"/>
+        <img src="https://cdn.uc.assets.prezly.com/5b79cc3a-6778-4bc7-a620-346a387dc95d/aspire-vero_16-03.jpg" alt="laptop Aspire" width="200" height="200" />
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMgt1Oxab1XfdPp7IFcFQtb3vklkub-H9Lsw&s" alt="laptop HP" width="200" height="200" />
+        <a href="https://bazara.co.mz/sm-a047fds-samsung-galaxy-a04s-64gb-4gb-ram-dual-sim-preto"><h4>SmartPhones</h4> </a>
+        <img src="https://cdn.bazara.co.mz/media/catalog/product/cache/bf49dd73f0c5af308fe168a7669f00fa/a/0/a04s_2_1.jpg" alt="phone samsung A04s"  width="200" height="200" />
+        <img src="https://cdn.bazara.co.mz/media/catalog/product/cache/bf49dd73f0c5af308fe168a7669f00fa/i/n/inst-samsung-a03-core-32gb-2gb.jpg" alt="phone samsung A03"  width="200" height="200" />
+        <img src="https://cdn.bazara.co.mz/media/catalog/product/cache/ea05800347c6b7bb78c005a60e973105/r/e/redmi_cindoca_1.jpg" alt="phone Redmi Note13"  width="200" height="200" />
+        <img src="https://cdn.bazara.co.mz/media/catalog/product/cache/bf49dd73f0c5af308fe168a7669f00fa/s/2/s23-1_1_3.jpg" alt="phone samsung A04s"  width="200" height="200" />
+       <a href="">  <h4> Tablets</h4></a>
+        <img src="" alt="phone samsung A04s"  width="200" height="200" />
+
+
+
+
+           
+        
+         
+       
 
     </main>
   )
